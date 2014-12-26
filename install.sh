051bc49b9c20db660af044e796fc91c2fcfb25cd
Kristijan Husak <husakkristijan@gmail.com>
2014-12-26 11:20:02 +0100

    Fix html snippet and install script.

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 #/bin/sh
-rm -rf ~/.vim ~/.vimrc && git clone git@github.com:gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim && ln -s $(pwd)/vimrc ~/.vimrc && ln -s $(pwd)/snippets ~/.vim/snippets && vim +PluginInstall +qall
+rm -rf ~/.vim ~/.vimrc && git clone git@github.com:gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim && ln -s $(pwd)/vimrc ~/.vimrc && ln -s $(pwd)/snippets ~/.vim/UltiSnips && vim +PluginInstall +qall
